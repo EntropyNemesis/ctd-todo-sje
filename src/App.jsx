3a3e81aacfc1f1ddc77270b02a8b4a8eae72152b
Stephanie Jane <stephaniejane@shopforage.com>
2026-07-07 18:32:07 -0400

    minor syntax tweaks based on AI Reviewer feedback

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import './App.css'
 
 function App() {
   
-  const toDoList = [
-    {id: 0, title: "prep wooden boat trim pieces"},
-    {id: 1, title: "acquire replacement trailer tires"},
-    {id: 2, title: "topcoat trim pieces"},
+  const todoList = [
+    {id: 1, title: "prep wooden boat trim pieces"},
+    {id: 2, title: "acquire replacement trailer tires"},
+    {id: 3, title: "topcoat trim pieces"},
   ]
   
   return (
 
     <div>
       <h1>My To-Do List</h1>
-      <ul>{toDoList.map(todo => <li key={todo.id}>{todo.title}</li>)}</ul>
+      <ul>{todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}</ul>
     </div>
   )   
 }
