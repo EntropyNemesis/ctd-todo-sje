82981094c9d29b76b2a143def4090db8ba30bcbd
Stephanie Jane <stephaniejane@shopforage.com>
2026-08-19 19:26:22 -0400

    Part 4: Updated App component for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css';
 import Header from './shared/Header.jsx'
 import TodosPage from './features/Todos/TodosPage.jsx'
 import Logon from './features/Logon.jsx'
+import {useState} from 'react';
 
 function App() {
-  
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
+
   return (
 
     <div>
-      <Header />
-      <Logon /><br/><br/>
-      <TodosPage />
+      <Header token={token} onSetEmail={setEmail} onSetToken={setToken} />
+      {token ? 
+        <TodosPage token={token} /> : 
+        <Logon onSetEmail={setEmail} onSetToken={setToken} />}
       
     </div>
   )   
