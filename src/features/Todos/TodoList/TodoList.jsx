94965ddefdb9b073962cc5555f24a6bc7822e811
Stephanie Jane <stephaniejane@shopforage.com>
2026-08-31 18:12:29 -0400

    removed console.log from memoized Todo list again

--- src/features/Todos/TodoList/TodoList.jsx
+++ src/features/Todos/TodoList/TodoList.jsx
@@ -3,7 +3,6 @@ import {useMemo} from 'react';
 
 function TodoList({todoList, onCompleteTodo, onUpdateTodo, dataVersion}) {
     const filteredTodoList = useMemo(() => {
-        console.log(`Recalculating filtered todos (v${dataVersion})`);
         return {
             version: dataVersion, 
             todos: todoList.filter(todo => todo.isCompleted === false)};
@@ -21,3 +20,5 @@ function TodoList({todoList, onCompleteTodo, onUpdateTodo, dataVersion}) {
 }
 
 export default TodoList;
+
+//console.log(`Recalculating filtered todos (v${dataVersion})`); removed from line 6
